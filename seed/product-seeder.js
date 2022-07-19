de0681aa740dd931dad26f2ba44af985780b3722
Lera Bondarchuk <lerabondarchuk38@gmail.com>
2022-07-19 19:38:08 +0300

    Using .length

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -18,35 +18,33 @@ const deleteData = () => {
 
 async function seedData() {
   try {
-    const query = await Product.find();
-    query.count(function (err, count) {
-      if (count === 0) {
-        let products = [];
-        for (let i = 0; i < 24; i++) {
-          let item = new Product({
-            imagePath: faker.image.food(640, 480, true),
-            title: faker.commerce.productName(),
-            description: faker.commerce.productDescription(),
-            price: faker.commerce.price(10, 90, 0),
-          });
-          products.push(item);
-        }
-        console.log("seedData!");
+    const items = await Product.find();
+    if (items.length === 0) {
+      let products = [];
+      for (let i = 0; i < 24; i++) {
+        let item = new Product({
+          imagePath: faker.image.food(640, 480, true),
+          title: faker.commerce.productName(),
+          description: faker.commerce.productDescription(),
+          price: faker.commerce.price(10, 90, 0),
+        });
+        products.push(item);
+      }
+      console.log("seedData!");
 
-        let done = 0;
-        for (let i = 0; i < products.length; i++) {
-          products[i].save(() => {
-            done++;
-            if (done === products.length) {
-              mongoose.disconnect();
-            }
-          });
-        }
-      } else console.log("Count is", count);
-    });
+      let done = 0;
+      for (let i = 0; i < products.length; i++) {
+        products[i].save(() => {
+          done++;
+          if (done === products.length) {
+            mongoose.disconnect();
+          }
+        });
+      }
+    }
   } catch (err) {
     console.log(err);
   }
-};
+}
 
 module.exports = seedData;
