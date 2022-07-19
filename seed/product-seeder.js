979f91679cadae4c9807905947689ec3e23ca3a8
Lera Bondarchuk <lerabondarchuk38@gmail.com>
2022-07-19 17:10:50 +0300

    Async/await product-seeder.js

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 const { faker } = require("@faker-js/faker");
 
-mongoose.connect("mongodb+srv://vercel-admin-user:QkFv0Usb0jQLTZjC@cluster0.43cr434.mongodb.net/shopping_cart?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://vercel-admin-user:QkFv0Usb0jQLTZjC@cluster0.43cr434.mongodb.net/shopping_cart?retryWrites=true&w=majority"
+);
 
 const deleteData = () => {
   Product.deleteMany({})
@@ -14,34 +16,37 @@ const deleteData = () => {
     });
 };
 
-const seedData = () => {
-  let products = [];
-  for (let i = 0; i < 24; i++) {
-    let item = new Product({
-      imagePath: faker.image.food(640, 480, true),
-      title: faker.commerce.productName(),
-      description: faker.commerce.productDescription(),
-      price: faker.commerce.price(10, 90, 0),
-    });
-    products.push(item);
-  }
-  console.log("seedData!");
+async function seedData() {
+  try {
+    const query = await Product.find();
+    query.count(function (err, count) {
+      if (count === 0) {
+        let products = [];
+        for (let i = 0; i < 24; i++) {
+          let item = new Product({
+            imagePath: faker.image.food(640, 480, true),
+            title: faker.commerce.productName(),
+            description: faker.commerce.productDescription(),
+            price: faker.commerce.price(10, 90, 0),
+          });
+          products.push(item);
+        }
+        console.log("seedData!");
 
-  let done = 0;
-  for (let i = 0; i < products.length; i++) {
-    products[i].save(() => {
-      done++;
-      if (done === products.length) {
-        mongoose.disconnect();
-      }
+        let done = 0;
+        for (let i = 0; i < products.length; i++) {
+          products[i].save(() => {
+            done++;
+            if (done === products.length) {
+              mongoose.disconnect();
+            }
+          });
+        }
+      } else console.log("Count is", count);
     });
+  } catch (err) {
+    console.log(err);
   }
 };
 
-
-const query = Product.find();
-query.count(function (err, count) {
-    if (err) console.log(err);
-    else if(count === 0) seedData();
-    else console.log("Count is", count);
-});
+module.exports = seedData;
